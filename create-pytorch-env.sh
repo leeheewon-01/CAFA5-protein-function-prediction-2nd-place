e10120f9f76bc23878e3c44bdb5f29831bad57ed
HEE-WON, LEE <81808758+leeheewon-01@users.noreply.github.com>
2025-10-18 02:32:45 +0900

    Update create-pytorch-env.sh

--- create-pytorch-env.sh
+++ create-pytorch-env.sh
@@ -8,5 +8,6 @@ conda create --solver=libmamba -p $1/pytorch-env python=3.9 -y
 conda activate $1/pytorch-env
 # conda install --solver=libmamba pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.8 -c pytorch -c nvidia -y
 conda install --solver=libmamba -c conda-forge cupy -y
+pip3 install torch torchvision
 pip install joblib tqdm pandas pyyaml pyarrow numba scikit-learn numpy scipy fair-esm
 pip install obonet pyvis transformers torchmetrics torchsummary sentencepiece psutil
